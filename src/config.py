a6ca471431c96cd5e2585e919fe28417e34c2a0f
nicholasjhana <nickashaw3@gmail.com>
2020-10-14 19:58:42 +0200

    default paths and file names

--- src/config.py
+++ src/config.py
@@ -1,9 +1,22 @@
 #config
 
 
-TRAINING_PATH = "../input/train_folds.csv"
+RAW_PATH = "../input/raw"
+RAW_FILE = "raw_data.csv"
+
+
+DATASET_PATH = "../input/dataset"
+TRAINING_FILE = "train_folds.csv"
+HOLDOUT_FILE = "test_holdout.csv"
+TEST_FILE = "test.csv"
+
+TRAINING_PATH = "../input/dataset/train_folds.csv"
 MODEL_OUTPUT = f"../models"
 N_KFOLDS = 5
 DEFAULT_MODEL = "tree-gini"
 
 
+
+
+SCALING = None
+DEFAULT_TEST_SIZE = 0.15
