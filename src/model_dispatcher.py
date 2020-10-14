7d6d5c03b2f9cde7f211d5cb2686cbb66e3ce808
nicholasjhana <nickashaw3@gmail.com>
2020-10-14 18:50:05 +0200

    added basic models to model dispatcher

---
+++ src/model_dispatcher.py
@@ -0,0 +1,21 @@
+from sklearn import tree, ensemble, linear_model, svm
+
+
+models = {
+    "tree-gini": tree.DecisionTreeClassifier(criterion="gini",
+                                         min_samples_split=20,
+                                         min_samples_leaf=20),
+    "tree-entropy": tree.DecisionTreeClassifier(criterion="gini",
+                                         min_samples_split=20,
+                                         min_samples_leaf=20),
+    "rf": ensemble.RandomForestClassifier(n_estimators= 20,
+                                           max_depth=18,
+                                           min_samples_split=20,
+                                           min_samples_leaf=20),
+    "logistic": linear_model.LogisticRegression(),
+    "svc": svm.SVC(),
+    "xgboost": None,
+    "lightgbm": None
+}
+
+
