b2afeb10b4e5d082c28562db8e1b4de0d73a499b
nicholasjhana <nickashaw3@gmail.com>
2020-10-18 14:26:51 +0200

    inital commit of preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,25 @@
+"""
+Preporcessing is the same across the train and test pipelines.
+
+Capabilities:
+Default preprocessing
+Feature engineering
+Feature selection
+
+Allows different feature creation and selection processed according to the selection of the pipeline.
+"""
+
+def preprocess():
+    pass
+
+
+def default_processing():
+    pass
+
+
+def feature_engineering():
+    pass
+
+def feature_selection():
+    pass
+
