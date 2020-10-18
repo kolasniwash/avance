ca6be584b8e740553b75338c97c16dde6df9132c
nicholasjhana <nickashaw3@gmail.com>
2020-10-18 14:23:45 +0200

    inital commit of standardize.py

---
+++ src/standardize.py
@@ -0,0 +1,9 @@
+"""
+Standarize.py standardizes the input dataframe to the specifications of the argparse
+
+Can take in any dataset and returns the standard column set for the processing pipeline.
+"""
+
+
+def stadard_input():
+    pass
