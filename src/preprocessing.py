c368934381834297e3586c26bfca07e341dbb9dc
nicholasjhana <nickashaw3@gmail.com>
2020-10-19 21:35:03 +0200

    added default processing to rename cols, target, and fillna to 0

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import argparse
+import os
+
+
+import config
 """
 Preporcessing is the same across the train and test pipelines.
 
@@ -9,17 +15,47 @@ Feature selection
 Allows different feature creation and selection processed according to the selection of the pipeline.
 """
 
-def preprocess():
-    pass
+def default_processing(raw_path):
+    """
+    Default_processing standardizes the columns
+
+    :return:
+    """
+    df = pd.read_csv(raw_path)
+
+    df.columns = df.columns.str.lower()
+    df = df.rename(columns={"status": "target"})  # these lines to be moved to a preprocessing stage.
+
+    df = df.fillna(0)
+
+    return df
 
 
-def default_processing():
-    pass
+
+def feature_engineering(df):
+    return df
+
+def feature_selection(df):
+    return df
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--raw_path", type=str, default=os.path.join(config.RAW_PATH,
+                                                                     config.RAW_FILE))
+    parser.add_argument("--save_path", type=str, default=os.path.join(config.DATASET_PATH,
+                                                                     config.PREPROCESSED_FILE))
 
 
-def feature_engineering():
-    pass
+    args = parser.parse_args()
+    raw_path = args.raw_path
+    save_path = args.save_path
 
-def feature_selection():
-    pass
+    df = default_processing(raw_path)
 
+    df = feature_engineering(df)
+
+    df = feature_selection(df)
+
+    df.to_csv(save_path, index=False)
