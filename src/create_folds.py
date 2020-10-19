82858d3abcfc5769663ff104640dba8844bfbc3e
nicholasjhana <nickashaw3@gmail.com>
2020-10-19 21:22:18 +0200

    modified to work with preprocessed dataset from preprocessing.py

--- src/create_folds.py
+++ src/create_folds.py
@@ -2,18 +2,16 @@ import os
 import pandas as pd
 import argparse
 from sklearn import model_selection
+import warnings
+warnings.filterwarnings('ignore')
 
 import config
 
-def dataset(folds, scaling, raw_path, save_path, test_size=config.DEFAULT_TEST_SIZE):
+def dataset(folds, preprocessed_path, save_directory, test_size=config.DEFAULT_TEST_SIZE):
 
-    df = pd.read_csv(os.path.join(raw_path, config.RAW_FILE))
+    df = pd.read_csv(preprocessed_path)
 
     df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-    df.columns = df.columns.str.lower()
-    df = df.rename(columns={"status": "target"}) #these lines to be moved to a preprocessing stage.
-    df = df.fillna(0)
-
     test_size = int(df.shape[0] * (1 - test_size))
 
     # train and test split
@@ -24,12 +22,6 @@ def dataset(folds, scaling, raw_path, save_path, test_size=config.DEFAULT_TEST_S
     test_len = test_df.shape[0]//2
     test_1 = test_df[:test_len]
     test_2 = test_df[test_len:]
-
-    if scaling=="min_max":
-        #apply minmax/standardscaler scaling
-        pass
-    elif scaling == "standard":
-        pass
 
     # add kfolds to the training set
     train_df['kfold'] = -1
@@ -42,9 +34,9 @@ def dataset(folds, scaling, raw_path, save_path, test_size=config.DEFAULT_TEST_S
         train_df.loc[v, "kfold"] = f
 
     # save datasets
-    train_df.to_csv(os.path.join(save_path, config.TRAINING_FILE), index=False)
-    test_1.to_csv(os.path.join(save_path, config.HOLDOUT_FILE), index=False)
-    test_2.to_csv(os.path.join(save_path, config.TEST_FILE), index=False)
+    train_df.to_csv(os.path.join(save_directory, config.TRAINING_FILE), index=False)
+    test_1.to_csv(os.path.join(save_directory, config.HOLDOUT_FILE), index=False)
+    test_2.to_csv(os.path.join(save_directory, config.TEST_FILE), index=False)
 
 
 if __name__ == "__main__":
@@ -52,13 +44,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--n_folds", type=int, default=config.N_KFOLDS)
-    parser.add_argument("--scaling", type=str, default=config.SCALING)
-    parser.add_argument("--raw_data_path", type=str, default=config.RAW_PATH)
-    parser.add_argument("--save_path", type=str, default=config.DATASET_PATH)
+    parser.add_argument("--preprocessed_path", type=str, default=os.path.join(config.DATASET_PATH, config.PREPROCESSED_FILE))
+    parser.add_argument("--save_directory", type=str, default=config.DATASET_PATH)
 
     args = parser.parse_args()
 
     dataset(args.n_folds,
-            args.scaling,
-            args.raw_data_path,
-            args.save_path)
+            args.preprocessed_path,
+            args.save_directory)
