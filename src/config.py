5bae9f14177c4a3dd32734750c75d5699a4dbd2c
nicholasjhana <nickashaw3@gmail.com>
2020-10-19 21:35:31 +0200

    updated paths raw, training

--- src/config.py
+++ src/config.py
@@ -1,17 +1,22 @@
 #config
 
-
+### RAW PATHS
 RAW_PATH = "../input/raw"
 RAW_FILE = "raw_data.csv"
 
-
+### TRAINING PATHS
+PREPROCESSED_FILE = "preprocessed_data.csv"
 DATASET_PATH = "../input/dataset"
 TRAINING_FILE = "train_folds.csv"
 HOLDOUT_FILE = "test_holdout.csv"
 TEST_FILE = "test.csv"
 
-TRAINING_PATH = "../input/dataset/train_folds.csv"
+### MODEL PATHS
 MODEL_OUTPUT = f"../models"
+
+
+
+
 N_KFOLDS = 5
 DEFAULT_MODEL = "tree-gini"
 
